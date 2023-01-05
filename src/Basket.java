3dfb2e269d4d85f620f37c15a0a8752a3acb1b91
Sergei Gridnev <sergeigr32@gmail.com>
2023-01-05 21:01:03 +0300

    First commit

---
+++ src/Basket.java
@@ -0,0 +1,79 @@
+import java.io.*;
+
+public class Basket {
+
+    protected int[] prices;
+
+    protected String[] goods;
+
+    protected int[] quantity;
+
+    protected int[] totalPrice = new int[3];
+
+    protected int sumProducts = 0;
+
+    protected Basket(int[] prices, String[] goods) {
+        this.prices = prices;
+        this.goods = goods;
+        this.quantity = new int[goods.length];
+    }
+
+    protected Basket() {
+    }
+
+    protected void addToCart(int productNum, int amount) {
+        quantity[productNum] = amount;
+        int currentPrice = prices[productNum] * quantity[productNum];
+        totalPrice[productNum] = currentPrice;
+        sumProducts = sumProducts + currentPrice;
+        System.out.println("Вы добавили " + goods[productNum] + " в кол-ве " + amount + " штук(и). Итоговая цена: " + totalPrice[productNum]);
+    }
+
+    protected void printCart() {
+        System.out.println("Ваша корзина:");
+        for (int i = 0; i < totalPrice.length; i++) {
+            if (totalPrice[i] == 0) {
+                continue;
+            }
+            System.out.println(goods[i] + " " + quantity[i] + " шт " + prices[i] + " " + "руб/шт " + totalPrice[i] + " руб " + "в сумме");
+        }
+        System.out.println("Итого: " + sumProducts + " руб");
+    }
+
+    protected void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(textFile);) {
+            out.println(goods.length);
+            for (int i = 0; i < goods.length; i++) {
+                out.println(goods[i] + "\t" + prices[i] + "\t" + quantity[i]);
+            }
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) throws IOException {
+        String[] goods = null;
+        int[] prices = null;
+        int[] quantity = null;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile));) {
+            int size = Integer.parseInt(bufferedReader.readLine());
+            goods = new String[size];
+            prices = new int[size];
+            quantity = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                String line = bufferedReader.readLine();
+                String[] parts = line.split("\t");
+                goods[i] = parts[0];
+                prices[i] = Integer.parseInt(parts[1]);
+                quantity[i] = Integer.parseInt(parts[2]);
+            }
+        }
+
+        Basket basket = new Basket();
+        basket.goods = goods;
+        basket.prices = prices;
+        basket.quantity = quantity;
+
+        return basket;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] products = {"Молоко", "Хлеб", "Гречка"};
+        int[] prices = {78, 50, 64};
+
+        System.out.println("Список возможных доваров для покупки");
+
+        for (int i = 0; i < products.length; i++) {
+            System.out.println(products[i] + " " + prices[i] + " руб/шт");
+        }
+
+        Basket basket1 = Basket.loadFromTxtFile(new File("basket.txt"));
+
+        Basket basket = new Basket(prices, products);
+
+        File textFile = new File("basket.txt");
+
+
+        while (true) {
+            System.out.println("Выберите товар и количество через пробел или введите `end`");
+            int productNumber;
+            int productCount;
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                basket.printCart();
+                break;
+            }
+
+            String[] parts = input.split(" ");
+            productNumber = Integer.parseInt(parts[0]) - 1;
+            productCount = Integer.parseInt(parts[1]);
+            basket.addToCart(productNumber, productCount);
+
+            basket.saveTxt(textFile);
+
+        }
+
+    }
+}
